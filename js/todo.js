453194920f2a38b745e921377f0be2005daa776a
jsgale <46535483+jsgale@users.noreply.github.com>
2019-03-09 23:09:23 -0700

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -35,7 +35,7 @@ function newElement() {
   document.getElementById("myInput").value = "";
 
   let span = document.createElement("SPAN");
-  let txt = document.createTextNode("\u00D7");
+  let txt = document.createTextNode("X");
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
